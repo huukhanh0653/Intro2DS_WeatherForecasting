370fcea71d56e5557013e796631ef59ff56d90e2
ductruonghoc <92839932+ductruonghoc@users.noreply.github.com>
2024-11-18 20:44:45 +0700

    V 1.0
    model.py: decision tree for predict labeled data (hmdx_label)

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
 import pandas as pd;
 from sklearn.model_selection import train_test_split;
 from sklearn.tree import DecisionTreeClassifier;
-from sklearn.metrics import accuracy_score 
-
-df = pd.read_csv('preProcessedData.csv');
+from sklearn.metrics import accuracy_score;
+import copy;
 
 class DecisionTreeModel:
     def __init__(self):
@@ -21,6 +20,7 @@ class DecisionTreeModel:
         if self.dataSet is None or self.preKey is None:
             print("Insert dataset and prediction key first.");
             return;
+        self.HighCorrelationRemove();
         preKey = self.preKey;
         df = self.dataSet;
         x = df.drop(preKey, axis=1);
@@ -46,8 +46,12 @@ class DecisionTreeModel:
         core = DecisionTreeClassifier();
         core.fit(x_train, y_train);
         self.core = core;
+        print("Fit finish successfully!");
 
     def Prediction(self):
+        if self.x_test is None or self.core is None:
+            print("Insert dataset, prediction key and fit first.");
+            return;
         x_test = self.x_test;
         y_pred = self.core.predict(x_test);
         self.y_pred = y_pred;
@@ -56,6 +60,51 @@ class DecisionTreeModel:
         return y_pred;
 
     def Evaluation(self):
+        if self.x_test is None or self.core is None:
+            print("Insert dataset, prediction key and predict first.");
+            return;
         y_test, y_pred = self.y_test, self.y_pred;
         self.accuracy = accuracy_score(y_test, y_pred);
         return self.accuracy;
+
+    def HighCorrelationRemove(self):
+        threshold = 0.75;
+        df = copy.deepcopy(self.dataSet);
+        columns = df.columns;
+        for label in columns:
+            if df[label].dtype == 'object':
+                df = df.drop(label, axis=1);
+        #corr matrix
+        corr_matrix = df.corr().abs();
+        #result set
+        result = copy.deepcopy(self.dataSet);
+        #columns of corr matrix
+        columns = corr_matrix.columns;
+        col_num = len(columns);
+        #loop through the columns
+        i = 0;
+        while i < col_num:
+            for j in range(0, i):
+                corr = corr_matrix.iloc[i, j];
+                if corr > threshold:
+                    #Drop from result
+                    result = result.drop(columns[i], axis=1);
+                    #Drop from both side of matrix
+                    corr_matrix = corr_matrix.drop(columns[i], axis=0);
+                    corr_matrix = corr_matrix.drop(columns[i], axis=1);
+                    #Drop the current column from list
+                    columns = columns.drop(columns[i]);
+                    col_num -= 1;
+                    #Back at current index
+                    i -= 1;
+                    break;
+            i += 1;
+        self.dataSet = result;
+
+df = pd.read_csv('preProcessedDataWithHmdx.csv');
+df = df.drop(['Year', 'Month', 'Day'], axis=1);
+dtm = DecisionTreeModel();
+dtm.AddDataSet(df);
+dtm.SetPredictionKey('HMDX_label');
+dtm.Fit();
+print("Prediction Set: ", dtm.Prediction());
